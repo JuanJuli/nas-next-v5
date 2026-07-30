91a02f0cab7fac7327a48f533718b8ab7630c72b
JuanJuli <juanjuliyanto79@gmail.com>
2026-07-30 14:56:31 +0700

    remove hardcode domain

--- src/service/tenant.ts
+++ src/service/tenant.ts
@@ -5,10 +5,8 @@ import { BASE_API_URL } from "@/utils/config";
 export async function getTenant(domain: string): Promise<DefaultApiResponse<LspByDomain> | null> {
   "use cache";
 
-  const developDomain = 'dev-abi.nasonline.id';
-
   return fetch(
-    `${BASE_API_URL}core/lsp/get-by-domain/${developDomain}`,
+    `${BASE_API_URL}core/lsp/get-by-domain/${domain}`,
     {
       next: { revalidate: 300 }, // 5 menit
     }
